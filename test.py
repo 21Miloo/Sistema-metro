d2632fdd42bf0491a51b6e89450320f3fb9266e8
21Miloo <camilo052110@gmail.com>
2025-10-22 23:13:22 -0500

    functions definition

---
+++ test.py
@@ -0,0 +1,42 @@
+# Functions to be tested
+
+# general stats
+
+
+def totalUsersMetro():  # numero total de usuarios en sistema metro
+    pass
+
+
+def numberTripsMetro():  # numero total de viajes realizados en el sistema metro
+    pass
+
+
+def rushHourMetro():  # hora pico del sistema metro
+    pass
+
+
+def totalPassengerMetro():  # numero total de pasajeros transportados en el sistema metro
+    pass
+
+
+def mostPopularRoutesMetro():  # rutas mas populares del sistema metro
+    pass
+
+
+# station stats
+
+
+def numberTripsStation(station_id):  # numero de viajes realizados en una estacion
+    pass
+
+
+def peakHoursStation(station_id):  # horas pico de una estacion
+    pass
+
+
+def OriginStation(station_id):  # estacion de origen mas popular de una estacion
+    pass
+
+
+def DestinationStation(station_id):  # estacion de destino mas popular de una estacion
+    pass
